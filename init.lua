38886ab409da05357fc347e732eb668703f788d7
hil4l <rachikhilal@gmail.com>
2025-05-06 23:51:59 +0200

    add Telescope + Treesitter

--- init.lua
+++ init.lua
@@ -10,22 +10,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	{ 'echasnovski/mini.files', version = '*',
-		keys = { 
-            { "<leader>fm",
-			function()
-				require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-			end,
-			desc = "Open mini.files (Directory of Current File)",
-			}
-		}
-	},
-    require 'plugins.rose_pine'
+	require 'plugins.minifiles',
+    require 'plugins.rosepine',
+    require 'plugins.treesitter',
+    require 'plugins.telescope',
 })
 
--- shift+tab keymap to < whole line
--- ctrl+l to select line (shift+V)
